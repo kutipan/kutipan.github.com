d865c5117e5cfb5b2b19be8b255d4d5a207155fe
Bagus <bagus@teknik.io>
2017-04-20 09:20:41 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-var bgcolorlist=new Array("#b6e6ea", "#fdd7d7", "#80FF80", "#EAEAFF", "#f2a8ff", "#F7F7F7", "#FFFFFF", "#caf8fc")
-
+var bgcolorlist=new Array("#636262", "#661e1e", "#663e1e", "##66661e", "##3c661e", "#1e6647", "#1e6666", 
+                          "#1e2866", "#441e66", "#601e66", "#661e45", "#661e36", "#661e1e", "#1e4566")
 document.body.style.background=bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
