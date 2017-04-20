2f60b5ae565afd8f360b50a8e8245eee2aff67eb
Bagus <bagus@teknik.io>
2017-04-20 08:37:43 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,3 @@
+var bgcolorlist=new Array("#b6e6ea", "#fdd7d7", "#80FF80", "#EAEAFF", "#f2a8ff", "#F7F7F7", "#FFFFFF", "#caf8fc")
+
+document.body.style.background=bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
