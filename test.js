1cf111895ec685758bbd92615800ab3adeaea0be
Bagus <bagus@teknik.io>
2017-04-21 14:08:01 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,1 @@
-var bgcolorlist=new Array("#636262","#661e1e","#663e1e","#66661e","#3c661e","#1e6647","#1e6666","#1e2866","#441e66","#601e66","#661e45","#661e36","#661e1e","#1e4566")
-
-document.body.style.background=bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)]
-
+var bgcolorlist=new Array("#6a6a6a","#636262","#661e1e","#663e1e","#66661e","#3c661e","#1e6647","#1e6666","#1e2866","#441e66","#601e66","#661e45","#661e36","#661e1e","#1e4566");document.body.style.background=bgcolorlist[Math.floor(Math.random()*bgcolorlist.length)];
